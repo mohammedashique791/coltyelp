5d564a90f61477be3c1097dc40eb89f2c7a9e4c0
mohammedashique791 <mohammedashiquevt791@gmail.com>
2024-01-29 19:59:14 +0530

    changed mongo url

--- app.js
+++ app.js
@@ -44,7 +44,7 @@ app.use(express.static(path.join(__dirname, 'public')))
 const secret = process.env.SECRET || 'thisshouldbeabettersecret!';
 
 const store = MongoStore.create({
-    mongoUrl: 'mongodb://127.0.0.1:27017/coltYelp',
+    mongoUrl: databaseUrl,
     touchAfter: 24 * 60 * 60,
     crypto: {
         secret: secret
