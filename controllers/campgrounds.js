805984c417cc41935f8903f69696bf6be206f7a5
mohammedashique791 <mohammedashiquevt791@gmail.com>
2024-01-29 18:05:38 +0530

    changed mapbox token spelling

--- controllers/campgrounds.js
+++ controllers/campgrounds.js
@@ -1,6 +1,6 @@
 const Campground = require('../models/campground');
 const mbxGeocoding = require("@mapbox/mapbox-sdk/services/geocoding");
-const mapBoxToken = process.env.MAPBOX_TOKEN;
+const mapBoxToken = process.env.mapbox_token;
 const geocoder = mbxGeocoding({ accessToken: mapBoxToken });
 const { cloudinary } = require("../cloudinary");
 
